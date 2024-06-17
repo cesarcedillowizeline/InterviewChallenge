8ae364b587c39a52779b9d5ea3310847701f279e
cesarcedillowizeline <119517156+cesarcedillowizeline@users.noreply.github.com>
2024-06-17 11:02:40 +0000

    adding launch files

--- API.InterviewChallenge/API.InterviewChallenge/Core/Domain/Interfaces/IBookingRepository.cs
+++ API.InterviewChallenge/API.InterviewChallenge/Core/Domain/Interfaces/IBookingRepository.cs
@@ -1,4 +1,4 @@
-﻿using API.InterviewChallenge.Adapters.Persistence;
+﻿using API.InterviewChallenge.Core.Adapters.Persistence;
 using API.InterviewChallenge.Core.Domain.Models;
 
 namespace API.InterviewChallenge.Core.Domain.Interfaces
