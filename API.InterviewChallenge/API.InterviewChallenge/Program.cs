a94488b2890738ea7871e71274b532623843cd5c
cesarcedillo <cesarcedillo@gmail.com>
2024-06-17 14:09:08 +0200

    including swagger

--- API.InterviewChallenge/API.InterviewChallenge/Program.cs
+++ API.InterviewChallenge/API.InterviewChallenge/Program.cs
@@ -1,12 +1,18 @@
 using API.InterviewChallenge.Core.Adapters.Persistence;
 using API.InterviewChallenge.Core.Domain.Interfaces;
 using API.InterviewChallenge.Core.Domain.Services;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "challenge", Version = "v1" });
+});
 
 //// Register the IBookingService and its implementation
 //builder.Services.AddScoped<IBookingService, BookingService>();
@@ -21,6 +27,18 @@ builder.Services.AddControllers();
 //builder.Services.AddSingleton<InMemoryDbContext>();
 
 var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Nombre de tu API V1");
+        // URL de la interfaz de usuario de Swagger (opcional)
+        c.RoutePrefix = "api"; // Configura la raíz de la URL donde se servirá Swagger UI, por ejemplo "/" para la raíz del sitio
+    });
+}
 
 // Configure the HTTP request pipeline.
 
